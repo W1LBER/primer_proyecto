b5becc88cf5bcf2d4957cb2d4861ef22012be2a8
Wilber García <wilber.garcia@outlook.es>
2020-09-19 22:44:37 -0300

    modificado:     app/views/layouts/application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/two'
+  get 'pages/three'
   root to: 'pages#one'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
